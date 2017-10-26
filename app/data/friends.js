02c18195301a89a49fb725725bce6666d525a294
tinyherocarrot <andrw.huang@gmail.com>
2017-10-26 10:03:22 -0500

    closestMatch logic is working, set extendedURL to true and now scores array is pushing half correctly

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,6 +16,38 @@ var friends =
 	      4,
 	      1
 	    ]
+	},
+	{
+	  "name":"Deanius",
+	  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	  "scores":[
+	      5,
+	      5,
+	      5,
+	      2,
+	      4,
+	      1,
+	      2,
+	      3,
+	      3,
+	      5
+	    ]
+	},
+	{
+	  "name":"Caroline",
+	  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	  "scores":[
+	      1,
+	      1,
+	      1,
+	      3,
+	      5,
+	      5,
+	      5,
+	      5,
+	      5,
+	      5
+	    ]
 	}
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,9 +18,8 @@ module.exports = function(app) {
 	//		This route will also be used to handle the compatibility logic.
 	app.post("/api/friends", function(req, res) {
 		var newFriend = req.body;
-		// console.log(newFriend);
-		// push to the imported data file HERE
-		friendsData.push(newFriend);
+		console.log(newFriend);
+		
 		// console.log(friendsData);
 
 		// compatibility logic HERE
@@ -32,22 +31,27 @@ module.exports = function(app) {
 			"totalDifference": 1000
 		};
 		// run through each person in data file
-		// friendsData.forEach(person => {
-		// 	var totalDifference = 0;
-		// 	console.log(`${person.name}'s scores: ${person.scores}`);
+		friendsData.forEach(person => {
+			var totalDifference = 0;
+			console.log(`${person.name}'s scores: ${person.scores}`);
+			console.log(`${newFriend.name}'s scores: ${newFriend.scores}`);
 			
-		// 	// and calculate total difference between newFriends scores and person's scores
-		// 	for (var i = 0; i < person.scores.length; i++) {
-		// 		totalDifference += Math.abs(person.scores[i]-newFriend.scores[i]);
-		// 	}
+			// and calculate total difference between newFriends scores and person's scores
+			for (var i = 0; i < 10; i++) {
+				totalDifference += Math.abs(person.scores[i]-newFriend.scores[i]);
+			}
+			console.log(totalDifference);
+			// if this person is more compatible, replace closestMatch with this person
+			if(totalDifference < closestMatch.totalDifference) {
+				closestMatch.person = person;
+				closestMatch.totalDifference = totalDifference;
+			}
 
-		// 	// if this person is more compatible, replace closestMatch with this person
-		// 	if(totalDifference < closestMatch.totalDifference) {
-		// 		closestMatch.person = person;
-		// 		closestMatch.totalDifference = totalDifference;
-		// 	}
-		// })
+		})
 
+		console.log(closestMatch);
+		// push to the imported data file HERE
+		friendsData.push(newFriend);	
 		// return JSON obj of closestMatch person to the front end
 		res.json("hi im an returned");
 	});
